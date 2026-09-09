8dc56bb56382113562b87d0733cbcee3847e1c45
ferrazmanu <ferraz.manuela@hotmail.com>
2026-09-09 15:05:54 -0300

    chore(config): initialize project structure, tooling and assets

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tableforge.com.br",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
