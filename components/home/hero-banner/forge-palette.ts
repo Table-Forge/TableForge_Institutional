ccc2eb8c53e742eb7eaed7793f9aaff3ad1a4df6
ferrazmanu <ferraz.manuela@hotmail.com>
2026-09-21 23:54:34 -0300

    style(home): center the forge ignition mark in the hero

--- components/home/hero-banner/forge-palette.ts
+++ components/home/hero-banner/forge-palette.ts
@@ -30,4 +30,4 @@ export const FORGE_STAGE = { width: 1920, height: 1080 } as const;
 export const FORGE_STAGE_CLASS =
   "absolute left-1/2 top-1/2 h-[1080px] w-[1920px] [transform:translate(calc(-50%_+_var(--forge-stage-shift,0px)),-50%)_scale(var(--forge-stage-scale,1))]";
 
-export const BURN_ORIGIN = { x: 960, y: 650 } as const;
+export const BURN_ORIGIN = { x: 960, y: 540 } as const;
